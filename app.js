e0c35b0ebe828cfeeb778a07030abdfe2541093a
Tatiana Eskina <horechek444@gmail.com>
2020-10-21 20:06:33 +0300

    feat: начало выполнения ПР 13

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const usersRoutes = require('./routes/users.js');
 const cardsRoutes = require('./routes/cards.js');
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRoutes);
---
+++ models/card.js
@@ -0,0 +1,35 @@
+import ObjectId from 'mongoose';
+
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /https?:\/\/w*\.?\w+\S+/.test(v);
+      },
+    },
+  },
+  owner: {
+    type: ObjectId,
+    required: true,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
